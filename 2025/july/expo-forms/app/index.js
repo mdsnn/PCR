e17097227bf0720bdf0b11c237ba7cea6c117571
mwiko <mwikosinyangwe@gmail.com>
2025-07-24 13:51:36 +0200

    working with react forms and yup

--- 2025/july/expo-forms/app/index.js
+++ 2025/july/expo-forms/app/index.js
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import LoginForm from ../components/LoginForm.js
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <LogingForm/>
     </View>
   );
 }
