79fd0f2272e3a93dffbb04238c648769377e54ba
mwiko <mwikosinyangwe@gmail.com>
2025-07-29 14:46:18 +0200

    forms

---
+++ 2025/july/shop/src/models/user.model.js
@@ -0,0 +1,18 @@
+const { pgTable, varchar, serial, timestamp } = require("drizzle-orm/pg-core");
+
+const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+const refreshTokens = pgTable("refresh_tokens", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id),
+  token: varchar("token", { length: 255 }).notNull().unique(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+module.exports = { users, refreshTokens };
