510ff1b61ab46516f9bbc880d472e1158cc477a9
mwiko <mwikosinyangwe@gmail.com>
2025-07-29 10:24:07 +0200

    forms

--- 2025/july/signin/App.js
+++ 2025/july/signin/App.js
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
 import React, { Component } from "react";
+import LoginScreen from "./components/LoginScreen";
 
 export class App extends Component {
   render() {
     return (
       <View>
-        <Text>App</Text>
+        <LoginScreen />
       </View>
     );
   }
