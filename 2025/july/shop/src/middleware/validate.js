1b625304a20666cfffddc51387a85a607eed2573
mwiko <mwikosinyangwe@gmail.com>
2025-07-29 17:00:48 +0200

    forms

--- 2025/july/shop/src/middleware/validate.js
+++ 2025/july/shop/src/middleware/validate.js
@@ -1,37 +1,21 @@
-const { validationResult } = require("express-validator");
-const {
-  registrationSchema,
-  loginSchema,
-  refreshTokenSchema,
-} = require("../schemas/user.schema");
+const validate = (schema) => {
+  return async (req, res, next) => {
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+      next();
+    } catch (error) {
+      const errors = error.inner.reduce((acc, err) => {
+        acc[err.path] = err.message;
+        return acc;
+      }, {});
 
-// Express-validator result handler
-const handleValidationErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
+      return res.status(400).json({
+        success: false,
+        message: "Validation error",
+        errors,
+      });
+    }
+  };
 };
 
-// Generic Yup validation middleware
-const validate = (schema) => async (req, res, next) => {
-  try {
-    await schema.validate(req.body, { abortEarly: false });
-    next();
-  } catch (error) {
-    const errors = error.inner.reduce((acc, curr) => {
-      acc[curr.path] = curr.message;
-      return acc;
-    }, {});
-    res.status(400).json({ errors });
-  }
-};
-
-module.exports = {
-  validate,
-  handleValidationErrors,
-  validateRegistration: validate(registrationSchema),
-  validateLogin: validate(loginSchema),
-  validateRefreshToken: validate(refreshTokenSchema),
-};
+module.exports = { validate };
