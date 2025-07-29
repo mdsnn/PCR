9fd2ad1a196370bd61e949b32c7472efb895ac5c
mwiko <mwikosinyangwe@gmail.com>
2025-07-29 15:17:46 +0200

    forms

--- 2025/july/shop/src/index.js
+++ 2025/july/shop/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authRoutes = require("./auth.routes");
+const authRoutes = require("./routes/auth.routes");
 const { authenticate } = require("../middleware/authenticate");
 const router = express.Router();
 
