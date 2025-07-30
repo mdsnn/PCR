017545926ed3bde261984efc49485aa3765125f5
mwiko <mwikosinyangwe@gmail.com>
2025-07-30 14:43:24 +0200

    working on clerk auth

--- 2025/july/shop/drizzle.config.js
+++ 2025/july/shop/drizzle.config.js
@@ -1,13 +1,13 @@
 // drizzle.config.js
-import { neon } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
-import * as schema from "./schema/users.js"
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schemas/users";
 
 export default {
-  schema,
+  schema: "./schemas", // ✅ path to folder or file
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
-}
+};
