cecc5b08cbefcf938f2fc1e7dbd703f0bbf54dfe
mwiko <mwikosinyangwe@gmail.com>
2025-07-30 13:56:33 +0200

    working on clerk auth

--- 2025/july/shop/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const { neon } = require("@neondatabase/serverless");
-const { drizzle } = require("drizzle-orm/neon-http");
-require("dotenv").config();
-
-const sql = neon(process.env.DATABASE_URL);
-const db = drizzle(sql);
-
-module.exports = { db, sql };
