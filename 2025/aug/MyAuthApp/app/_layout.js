9d72e177c0827b2087c67bbd564040bbfb158520
mwiko <mwikosinyangwe@gmail.com>
2025-08-03 17:34:56 +0200

    working with the laravel api

--- 2025/aug/MyAuthApp/app/_layout.js
+++ 2025/aug/MyAuthApp/app/_layout.js
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
+  );
 }
