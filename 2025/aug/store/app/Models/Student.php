c1c054f89a68c8fde67cfc15b3ad01354977d10e
mwiko <mwikosinyangwe@gmail.com>
2025-08-03 20:33:44 +0200

    good days

--- 2025/aug/store/app/Models/Student.php
+++ 2025/aug/store/app/Models/Student.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
