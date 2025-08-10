c75a0966c3a1a1d09007605a9d7b298f4202c096
mwiko <mwikosinyangwe@gmail.com>
2025-08-10 12:56:15 +0200

    all in one

---
+++ august/button/app/index.js
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+import FloatingActionGamepad from "../components/FloatingActionGamepad";
+
+export default function Index() {
+  return (
+    <FloatingActionGamepad/>
+  );
+}
