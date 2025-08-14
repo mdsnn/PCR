056f778bba256766c979e73e25124ea3cee95312
mwiko <mwikosinyangwe@gmail.com>
2025-08-14 17:21:58 +0200

    some stuff

--- 2025/july/madam/src/middleware/validate.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (schema) => async (req, res, next) => {
-  try {
-    await schema.validate(req.body, { abortEarly: false });
-    next();
-  } catch (err) {
-    res.status(400).json({ errors: err.errors });
-  }
-};
