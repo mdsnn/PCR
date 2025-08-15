42ddb0d116c149a677db90601b1ce211fbd97cf2
mdsnn <mwikosinyangwe@gmail.com>
2025-08-15 11:49:56 +0200

    nativewind

--- 2025/aug/shop/tailwind.config.js
+++ 2025/aug/shop/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./App.js"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
