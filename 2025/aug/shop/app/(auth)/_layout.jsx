257944d5c5135386555cc791b62334fcdf06db8d
mdsnn <mwikosinyangwe@gmail.com>
2025-08-16 14:18:47 +0200

    getters

--- 2025/aug/shop/app/(auth)/_layout.jsx
+++ 2025/aug/shop/app/(auth)/_layout.jsx
@@ -2,9 +2,9 @@ import { Stack } from "expo-router";
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" />
-      <Stack.Screen name="register" />
+    <Stack>
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="register" options={{ headerShown: false }} />
     </Stack>
   );
 }
