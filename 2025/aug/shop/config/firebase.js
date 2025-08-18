f91b87d9ef39e4228a23e25d4d66ba30c8e5d62b
mdsnn <mwikosinyangwe@gmail.com>
2025-08-18 12:39:25 +0200

    authz

---
+++ 2025/aug/shop/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAW6oo9LIJXOcg7CoT8xr6lz0V1leBLb2s",
+  authDomain: "shop-ed956.firebaseapp.com",
+  projectId: "shop-ed956",
+  storageBucket: "shop-ed956.firebasestorage.app",
+  messagingSenderId: "611897282142",
+  appId: "1:611897282142:web:13cce58417cfd43ca2aecf",
+  measurementId: "G-5PJTN9ER8Y",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
