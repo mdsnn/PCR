034a7506ffbf4f791f18b63d5a326aa75dc617dc
mdsnn <mwikosinyangwe@gmail.com>
2025-08-25 17:34:13 +0200

    stuff

--- 2025/aug/store/app/index.jsx
+++ 2025/aug/store/app/index.jsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text variant="headlineMedium">📦 FastAPI Client</Text>
+      <Link href="/users" asChild>
+        <Button mode="contained" style={{ marginTop: 20 }}>
+          Manage Users
+        </Button>
+      </Link>
+      <Link href="/products" asChild>
+        <Button mode="contained" style={{ marginTop: 10 }}>
+          Manage Products
+        </Button>
+      </Link>
+      <Link href="/orders" asChild>
+        <Button mode="contained" style={{ marginTop: 10 }}>
+          Manage Orders
+        </Button>
+      </Link>
     </View>
   );
 }
