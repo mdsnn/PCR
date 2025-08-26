cae25fa56ff2d1730d9147392283d3d80adcc8f6
mdsnn <mwikosinyangwe@gmail.com>
2025-08-26 15:07:29 +0200

    express

--- 2025/aug/shop/index.js
+++ 2025/aug/shop/index.js
@@ -11,10 +11,10 @@ app.use(cors());
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: process.env.DB_USER || "your_username",
+  user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "your_database",
-  password: process.env.DB_PASSWORD || "your_password",
+  database: process.env.DB_NAME || "store",
+  password: process.env.DB_PASSWORD || "chama",
   port: process.env.DB_PORT || 5432,
 });
 
