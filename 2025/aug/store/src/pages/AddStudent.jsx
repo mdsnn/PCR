0a99b6186bf3f10873c0ed50e01f087437e12eb2
mdsnn <mwikosinyangwe@gmail.com>
2025-08-26 16:21:46 +0200

    ..

---
+++ 2025/aug/store/src/pages/AddStudent.jsx
@@ -0,0 +1,10 @@
+import StudentForm from "../components/StudentForm";
+
+export default function AddStudent() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Add Student</h1>
+      <StudentForm />
+    </div>
+  );
+}
