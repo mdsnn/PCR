94116ef30ffb3d9f926d6a35fdbf9318763eb81a
mdsnn <mwikosinyangwe@gmail.com>
2025-08-26 16:22:21 +0200

    ..

--- 2025/aug/store/src/App.jsx
+++ 2025/aug/store/src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddStudent from "./pages/AddStudent";
+import EditStudent from "./pages/EditStudent";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddStudent />} />
+        <Route path="/edit/:id" element={<EditStudent />} />
+      </Routes>
+    </Router>
+  );
 }
