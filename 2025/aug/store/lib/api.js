444ced711f047ec0dfbf69c2390d0a7366f1a726
mdsnn <mwikosinyangwe@gmail.com>
2025-08-26 14:32:02 +0200

    hello

--- 2025/aug/store/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// ⚠️ Replace localhost with LAN IP if testing on device
-const api = axios.create({
-  baseURL: "http://192.168.141.233:8000",
-});
-
-export default api;
--- 2025/shop/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Load DB URL from environment variable or fallback
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:chama@localhost:5432/store"
-)
