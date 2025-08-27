c7520555ae6d2f3ac99da1094d0dba1108c7eff8
mdsnn <mwikosinyangwe@gmail.com>
2025-08-27 14:43:34 +0200

    ..

--- 2025/aug/shop/resources/js/pages/welcome.tsx
+++ 2025/aug/shop/resources/js/pages/welcome.tsx
@@ -1,7 +1,9 @@
 export default function Welcome() {
     return (
         <>
-            <h3>what about you?</h3>
+            <h3 className="mb-4 rounded-lg bg-gradient-to-r from-blue-50 to-indigo-50 p-4 text-2xl font-bold text-gray-800 shadow-md transition-shadow duration-300 hover:shadow-lg">
+                what about you?
+            </h3>
         </>
     );
 }
