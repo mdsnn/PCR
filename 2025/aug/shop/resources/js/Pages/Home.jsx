ff0eb234146f2bb145e93949caa3be91f261a6de
mdsnn <mwikosinyangwe@gmail.com>
2025-08-27 13:08:31 +0200

    stuff

---
+++ 2025/aug/shop/resources/js/Pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1 className="text-2xl font-bold">Hello from Inertia + React 🎉</h1>
+    );
+}
--- 2025/aug/shop/routes/web.php
+++ 2025/aug/shop/routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    return Inertia::render('Home');
 });
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
