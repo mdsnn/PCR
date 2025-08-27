85dd7211b2c57fffb1c16345a668d6e3210ba9b7
mdsnn <mwikosinyangwe@gmail.com>
2025-08-27 22:34:55 +0200

    ..

--- 2025/aug/shop/resources/js/app.tsx
+++ 2025/aug/shop/resources/js/app.tsx
@@ -1,5 +1,4 @@
 import '../css/app.css';
-import './bootstrap'; // Add this if you need the bootstrap import
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
