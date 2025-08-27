556d30447bfe06e081cf73c7773a80e9766d3aa4
mdsnn <mwikosinyangwe@gmail.com>
2025-08-27 14:33:28 +0200

    ..

---
+++ 2025/aug/shop/resources/js/pages/test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h2>Test Page</h2>
+            <p>This is another route rendered by Inertia 🚀</p>
+        </div>
+    );
+}
--- 2025/aug/shop/routes/web.php
+++ 2025/aug/shop/routes/web.php
@@ -6,3 +6,8 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/test', function () {
+    return Inertia::render('test'); // this will look for resources/js/pages/Test.jsx
+})->name('test');
+
