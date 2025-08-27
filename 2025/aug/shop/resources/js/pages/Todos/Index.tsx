e8098d78585d62d56f69ffe52fd4a085322c59f0
mdsnn <mwikosinyangwe@gmail.com>
2025-08-27 15:33:03 +0200

    ..

--- 2025/aug/shop/resources/js/pages/Todos/Index.tsx
+++ 2025/aug/shop/resources/js/pages/Todos/Index.tsx
@@ -1,9 +1,27 @@
 import { useForm } from '@inertiajs/react';
+import { FormEvent } from 'react';
 
-export default function Todos({ todos }) {
-    const { data, setData, post, reset } = useForm({ title: '' });
+// Define the Todo type
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
-    const submit = (e) => {
+// Define the component props type
+interface TodosProps {
+    todos: Todo[];
+}
+
+// Define the form data type
+interface TodoFormData {
+    title: string;
+}
+
+export default function Todos({ todos }: TodosProps) {
+    const { data, setData, post, reset } = useForm<TodoFormData>({ title: '' });
+
+    const submit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         post(route('todos.store'), {
             onSuccess: () => reset('title'),
@@ -30,16 +48,14 @@ export default function Todos({ todos }) {
 
             {/* Todo list */}
             <ul className="space-y-2">
-                {todos.map((todo) => (
+                {todos.map((todo: Todo) => (
                     <li key={todo.id} className="flex items-center justify-between rounded border p-2">
                         <div className={`flex-1 ${todo.completed ? 'text-gray-400 line-through' : ''}`}>{todo.title}</div>
-
                         <div className="flex gap-2">
                             <form method="post" action={route('todos.toggle', todo.id)}>
                                 <input type="hidden" name="_method" value="patch" />
                                 <button className="rounded bg-yellow-400 px-2 py-1 text-sm">{todo.completed ? 'Undo' : 'Done'}</button>
                             </form>
-
                             <form method="post" action={route('todos.destroy', todo.id)}>
                                 <input type="hidden" name="_method" value="delete" />
                                 <button className="rounded bg-red-500 px-2 py-1 text-sm text-white">Delete</button>
