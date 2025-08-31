5e93a0148e1068a4a2bad8235d72f99fc4b47cfb
mdsnn <mwikosinyangwe@gmail.com>
2025-08-31 18:58:19 +0200

    ..

--- 2025/aug/shop/database/migrations/2025_08_31_165619_create_profiles_table.php
+++ 2025/aug/shop/database/migrations/2025_08_31_165619_create_profiles_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // linked to users
+            $table->string('username')->unique()->nullable(); // public handle
+            $table->string('bio')->nullable();
+            $table->string('avatar')->nullable(); // profile image
+            $table->string('location')->nullable();
+            $table->string('phone')->nullable();
+            $table->date('birthdate')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('profiles');
