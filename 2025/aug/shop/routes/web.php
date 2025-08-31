3876c243e364c03913bc8caa77a9a6a453dc8080
mdsnn <mwikosinyangwe@gmail.com>
2025-08-31 19:12:21 +0200

    ..

--- 2025/aug/shop/routes/web.php
+++ 2025/aug/shop/routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\ProfileController;
+
+
 // Guest routes (unauthenticated users)
 Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
@@ -34,6 +37,10 @@ Route::middleware('auth')->group(function () {
     Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
     Route::patch('/todos/{todo}/toggle', [TodoController::class, 'toggle'])->name('todos.toggle');
     Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
 });
 
 // Test route (you might want to protect this too)
@@ -58,6 +65,6 @@ Route::get('/cart', function () {
 Route::get('/chat', function () {
     return Inertia::render('ChatPage');
 })->name('chat');
-Route::get('/profile', function () {
+Route::get('/profiles', function () {
     return Inertia::render('ProfilePage');
-})->name('profile');
+})->name('profiles');
