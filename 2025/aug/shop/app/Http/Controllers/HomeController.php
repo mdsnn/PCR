7de171f124407d821ce919f2a1b128cb628eb436
mdsnn <mwikosinyangwe@gmail.com>
2025-09-01 12:08:59 +0200

    ..

--- 2025/aug/shop/app/Http/Controllers/HomeController.php
+++ 2025/aug/shop/app/Http/Controllers/HomeController.php
@@ -9,14 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $todos = auth()->user()->todos()
-            ->orderBy('created_at', 'desc')
-            ->take(10) // Show latest 10 todos on home
-            ->get();
-
-        return Inertia::render('Home', [
-            'user' => auth()->user(),
-            'todos' => $todos,
-        ]);
+        return Inertia::render('Home');
     }
 }
