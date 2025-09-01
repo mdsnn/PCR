1439b6c6570a24fddbfa21f295a63d60bdbd6bad
mdsnn <mwikosinyangwe@gmail.com>
2025-09-01 16:48:10 +0200

    ..

--- 2025/aug/shop/app/Http/Middleware/HandleInertiaRequests.php
+++ 2025/aug/shop/app/Http/Middleware/HandleInertiaRequests.php
@@ -51,7 +51,7 @@ class HandleInertiaRequests extends Middleware
                 'location' => $request->url(),
             ],
             'flash' => [
-                'message' => fn () => $request->session()->get('message'),
+                'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
             ],
         ];
