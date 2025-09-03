32890a1304ea93e8991bcfb4b3a2bb01f1dee3ab
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 18:10:29 +0200

    ..

---
+++ 2025/sept/shop/resources/js/pages/Dashboard/Grocery.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Grocery Store Dashboard 🥦</h1>
+        <p>Welcome, manage your grocery products and supplies here.</p>
+    </div>
+</template>
