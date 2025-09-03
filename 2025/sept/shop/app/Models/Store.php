5310bd6c6c3f7784ef7da5da4169bd2aa6f04ce5
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 17:09:46 +0200

    ..

---
+++ 2025/sept/shop/app/Models/Store.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'type',
+    ];
+
+    /**
+     * Relationship: A store belongs to a user (the owner).
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
