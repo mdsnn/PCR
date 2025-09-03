ee73035e5a89838329cd60f0670bcd0632190ef6
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 18:09:26 +0200

    ..

---
+++ 2025/sept/shop/resources/js/pages/Dashboard/Bakery.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Bakery Store Dashboard �</h1>
+    <p>Welcome, manage your baked goods and supplies here.</p>
+  </div>
+</template>
