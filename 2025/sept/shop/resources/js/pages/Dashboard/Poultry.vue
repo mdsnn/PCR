e37b12692941bda88c449adecca84598c17b834e
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 18:10:54 +0200

    ..

---
+++ 2025/sept/shop/resources/js/pages/Dashboard/Poultry.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Poultry Store Dashboard 🐔</h1>
+        <p>Welcome, manage your poultry products and supplies here.</p>
+    </div>
+</template>
