c71715dcf8153a62adad785451d1b29c4f2e890b
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 18:10:02 +0200

    ..

---
+++ 2025/sept/shop/resources/js/pages/Dashboard/Coffee.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Coffee Shop Dashboard ☕</h1>
+        <p>Welcome, manage your coffee products and supplies here.</p>
+    </div>
+</template>
