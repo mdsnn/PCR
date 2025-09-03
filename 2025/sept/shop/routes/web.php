a2488dea8d2e97d95a62dae9d894addd58a6d16b
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 16:49:06 +0200

    ..

--- 2025/sept/shop/routes/web.php
+++ 2025/sept/shop/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\MagicLinkController;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\OnboardingController;
 // Public routes
 Route::get('/login', [MagicLinkController::class, 'showLogin'])->name('login');
 Route::post('/magic-link', [MagicLinkController::class, 'sendMagicLink'])->name('magic-link.send');
@@ -31,8 +31,15 @@ Route::get('/login', [MagicLinkController::class, 'showLogin'])->name('login');
 Route::post('/magic-link', [MagicLinkController::class, 'sendMagicLink'])->name('magic-link.send');
 Route::get('/magic-link/verify/{token}', [MagicLinkController::class, 'verify'])->name('magic-link.verify');
 
+
+
+
 // Protected routes
 Route::middleware('auth')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::post('/logout', [MagicLinkController::class, 'logout'])->name('logout');
+    Route::get('/onboarding', [OnboardingController::class, 'start'])->name('onboarding.start');
+    Route::post('/onboarding/choose-role', [OnboardingController::class, 'chooseRole'])->name('onboarding.chooseRole');
+    Route::post('/onboarding/complete', [OnboardingController::class, 'complete'])->name('onboarding.complete');
+
 });
