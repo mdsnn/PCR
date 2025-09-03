c86be7bdd90423ec34cbdd7520bbc7ca30403a38
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 14:53:36 +0200

    ..

--- 2025/sept/shop/routes/web.php
+++ 2025/sept/shop/routes/web.php
@@ -2,14 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\MagicLinkController;
+use App\Http\Controllers\HomeController;
+
+// Public routes
+Route::get('/login', [MagicLinkController::class, 'showLogin'])->name('login');
+Route::post('/magic-link', [MagicLinkController::class, 'sendMagicLink'])->name('magic-link.send');
+Route::get('/magic-link/verify/{token}', [MagicLinkController::class, 'verify'])->name('magic-link.verify');
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [MagicLinkController::class, 'logout'])->name('logout');
+});
 
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-})->name('home');
 Route::get('/login', function () {
     return Inertia::render('Login');
 })->name('login');
 Route::get('/map', function () {
     return Inertia::render('Map');
 })->name('map');
+
+
+
+// Public routes
+Route::get('/login', [MagicLinkController::class, 'showLogin'])->name('login');
+Route::post('/magic-link', [MagicLinkController::class, 'sendMagicLink'])->name('magic-link.send');
+Route::get('/magic-link/verify/{token}', [MagicLinkController::class, 'verify'])->name('magic-link.verify');
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [MagicLinkController::class, 'logout'])->name('logout');
+});
