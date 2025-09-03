6f48bd8f1bd09cf8fe2b1dd66f4713bba1e767a0
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 16:57:14 +0200

    ..

--- 2025/sept/shop/database/migrations/2025_09_03_145551_create_stores_table.php
+++ 2025/sept/shop/database/migrations/2025_09_03_145551_create_stores_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('type'); // e.g., fashion, electronics, etc.
             $table->timestamps();
         });
     }
