826062c4caa11197842a20d28e6b57746fa09223
mdsnn <mwikosinyangwe@gmail.com>
2025-09-03 18:08:23 +0200

    ..

---
+++ 2025/sept/shop/resources/js/pages/Dashboard/Farm.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Farm Store Dashboard 🌾</h1>
+        <p>Welcome, manage your crops and supplies here.</p>
+    </div>
+</template>
